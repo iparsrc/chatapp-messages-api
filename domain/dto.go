621af2a83fbc8f32de8890deadf2fbe8eb435e72
parsa <akbariparsa1209@gmail.com>
2020-08-13 20:36:05 +0430

    Changed Message ID type to string.

--- domain/dto.go
+++ domain/dto.go
@@ -1,14 +1,10 @@
 package domain
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 type Message struct {
-	ID          primitive.ObjectID `json:"id" bson:"_id"`
-	Seen        bool               `json:"seen" bson:"seen"`       // notifications.
-	Sender      string             `json:"sender" bson:"sender"`   // user.
-	Reciver     string             `json:"reciver" bson:"reciver"` // group or user.
-	Message     string             `json:"message" bson:"message"`
-	DateCreated int64              `json:"dateCreated" bson:"dateCreated"` // Unix time.
+	ID          string `json:"id" bson:"_id"`          // Sender + DateCreated.
+	Seen        bool   `json:"seen" bson:"seen"`       // notifications.
+	Sender      string `json:"sender" bson:"sender"`   // user.
+	Reciver     string `json:"reciver" bson:"reciver"` // group or user.
+	Message     string `json:"message" bson:"message"`
+	DateCreated int64  `json:"dateCreated" bson:"dateCreated"` // Unix time.
 }
